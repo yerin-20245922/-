a10d9bf75b3b69a58fbb80d3db7f7539cfecfcb6
ing <may3748@naver.com>
2024-12-19 16:33:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,12 @@ const toogleBtn = document.querySelector('.navbar__toogle');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
-const priceSort = document.querySelector('.container__price');
 const menuSort = document.querySelector('.container__menu');
-const placeSort = document.querySelector('.container__place');
 
-const price = document.querySelector('#price');
 const block = document.querySelector('#menu');
-const place = document.querySelector('#place');
 const random = document.querySelector('#random');
 
+var menuArray = new Array();
 var menuArray = new Array();
 menuArray[0] = "커피";
 menuArray[1] = "몸 건강을 위해 디카페인";
@@ -26,27 +23,15 @@ toogleBtn.addEventListener('click', function () {
     icons.classList.toggle('active');
 });
 
-price.addEventListener('click', function () {
-    priceSort.classList.add('active');
-    menuSort.classList.remove('active');
-    placeSort.classList.remove('active');
-});
 
 block.addEventListener('click', function () {
-    priceSort.classList.remove('active');
     menuSort.classList.add('active');
     placeSort.classList.remove('active');
 });
 
-place.addEventListener('click', function () {
-    priceSort.classList.remove('active');
-    menuSort.classList.remove('active');
-    placeSort.classList.add('active');
-});
 
 random.addEventListener('click', function () {
     let imgNum = Math.round(Math.random() * 7);
     let selected = menuArray[imgNum];
     alert("오늘은 " + selected + " 어떠신가요?");
 });
-
